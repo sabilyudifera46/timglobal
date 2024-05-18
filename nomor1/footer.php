11cc65810eabc397491b662d97f3a4e5d094399d
sabilyudifera46 <sabilyudifera46@gmail.com>
2024-05-18 12:57:07 +0700

    Delete nomor1 directory

--- nomor1/footer.php
+++
@@ -1,6 +0,0 @@
-<footer style="position:relative; bottom: 0; width: 100%; background-color:#0C1D32;">
-    <div id="selina_header_0">PT. DKM</div>
-    <div id="selina_header_1"><a class="selina_footer" href="<?php echo $path ?>menu_services.php" >Services</a>&nbsp&nbsp|&nbsp&nbsp<a class="selina_footer" href="<?php echo $path ?>menu_kontak.php">Contact</a>&nbsp&nbsp|&nbsp&nbsp<a class="selina_footer" href="<?php echo $path ?>menu_about.php">About</a></div>
-    <div id="selina_header_1">Support@dwipakharismamitra.com</div>
-    <div id="selina_header_2">Copyright 2021 © Dwipa Kharisma Mitra. All Rights Reserved.</div>
-</footer>
