bab584c79406150cc35bcb93dd29cd791cfbeb66
sabilyudifera46 <sabilyudifera46@gmail.com>
2024-05-18 13:05:55 +0700

    Add files via upload

---
+++ nomor1/contoh.php
@@ -0,0 +1,36 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+<div class="row">
+  <div class="col-md-6">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="Image/IMG_3559.JPG" id="gambar_carousel1" height="100%" width="100%">
+            </div>
+            <div class="carousel-item">
+            <img src="Image/IMG_3557.JPG" id="gambar_carousel1"  height="100%" width="100%">
+            </div>
+            <div class="carousel-item">
+            <img src="Image/IMG_3560.JPG" id="gambar_carousel1"  height="100%" width="100%">
+            </div>
+            <div class="carousel-item">
+            <img src="Image/IMG_3576.JPG" id="gambar_carousel1"  height="100%" width="100%">
+            </div>
+            <div class="carousel-item">
+            <img src="Image/IMG_3562.JPG" id="gambar_carousel1"  height="100%" width="100%">
+            </div>
+        </div>
+      <button type="button" class="carousel-control-prev" data-bs-target="#carouselExampleControls" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      </button>
+      <button type="button" class="carousel-control-next" data-bs-target="#carouselExampleControls" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      </button>
+    </div>
+  </div>
+  <div class="col-md-6">
+
+  </div>
+</div>
